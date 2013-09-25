17bdd0c82a1e76f251bdf2f57808087242ca67d2
TheEskimo <caffrey.sean@gmail.com>
2013-09-25 22:28:32 +0100

    search form alert

--- src/main/myScript.js
+++ src/main/myScript.js
@@ -5,11 +5,24 @@ function check(form)/*function to check userid & password*/
 /*the following code checks whether the entered userid and password are matching*/
 if(form.userid.value == "sean@email.com" && form.pswrd.value == "pass1word")
 {
-window.open('http://www.seancaffrey.vacau.com/')/*opens the target page while Id & password matches*/
+window.open('todolist.html')/*opens the target page while Id & password matches*/
 }
 else 
 {
 alert("Error Password or Username")/*displays error message*/
 }
 }
+
+function myFunction(name,job)
+{
+alert("Welcome " + name + ", the " + job);
+}
+
+  function updateTxt()
+  {  
+    var field1 = document.getElementById('textfield').value;  
+    var field2 = document.getElementById('select').options[document.getElementById('select').selectedIndex].value;  
+    alert(field1+'&'+field2);
+
+    }  
 
